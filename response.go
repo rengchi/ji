51c774447466706779298fbab6960e0944b7de2a
rengchi <serendipity@wufujinri.com>
2025-02-12 19:39:16 +0800

    html 转义相关

--- response.go
+++ response.go
@@ -10,12 +10,13 @@ import (
 
 // Resp 定义响应结构体，Data字段使用空接口interface{}表示可以接受任意类型数据
 type Resp struct {
-	Code            int         `json:"code"`                // 接口返回的状态码
-	Status          bool        `json:"status"`              // 请求处理状态，true表示成功，false表示失败
-	Message         string      `json:"message,omitempty"`   // 返回的提示信息，若无可省略
-	Data            interface{} `json:"data,omitempty"`      // 返回的实际数据，若为空则不返回该字段
-	ExecTime        int64       `json:"exec_time,omitempty"` // 请求处理时间，单位：毫秒
-	IncludeExecTime bool        `json:"-"`                   // 控制是否添加执行时间字段
+	Code            int         `json:"code"`                      // 接口返回的状态码
+	Status          bool        `json:"status"`                    // 请求处理状态，true表示成功，false表示失败
+	Message         string      `json:"message,omitempty"`         // 返回的提示信息，若无可省略
+	Data            interface{} `json:"data,omitempty"`            // 返回的实际数据，若为空则不返回该字段
+	ExecTime        int64       `json:"exec_time,omitempty"`       // 请求处理时间，单位：毫秒
+	IncludeExecTime bool        `json:"-"`                         // 控制是否添加执行时间字段（用于内部控制，不出现在 JSON 输出中）
+	HTMLIsEscaped   bool        `json:"html_is_escaped,omitempty"` // 是否转义HTML
 }
 
 // WriteResponse 用于写入 HTTP 响应
@@ -33,6 +34,9 @@ func WriteResponse(w http.ResponseWriter, statusCode int, resp Resp) {
 		resp.ExecTime = time.Since(start).Milliseconds()
 	}
 
+	// 根据 HTMLIsEscaped 字段判断是否需要转义 HTML
+	escapeHTML := !resp.HTMLIsEscaped // 默认为 true，表示需要转义 HTML
+
 	// 判断客户端是否支持 Gzip 压缩
 	if strings.Contains(w.Header().Get("Accept-Encoding"), "gzip") {
 		// 启用 Gzip 压缩
@@ -42,6 +46,7 @@ func WriteResponse(w http.ResponseWriter, statusCode int, resp Resp) {
 
 		// 使用 json.NewEncoder 直接写入响应体
 		encoder := json.NewEncoder(gz)
+		encoder.SetEscapeHTML(escapeHTML) // 设置 HTML 转义
 		if err := encoder.Encode(resp); err != nil {
 			// 处理序列化错误
 			http.Error(w, "JSON 编码错误", http.StatusInternalServerError)
@@ -50,6 +55,7 @@ func WriteResponse(w http.ResponseWriter, statusCode int, resp Resp) {
 	} else {
 		// 如果不支持 Gzip 压缩，直接写入响应体
 		encoder := json.NewEncoder(w)
+		encoder.SetEscapeHTML(escapeHTML) // 设置 HTML 转义
 		if err := encoder.Encode(resp); err != nil {
 			// 处理序列化错误
 			http.Error(w, "JSON 编码错误", http.StatusInternalServerError)
